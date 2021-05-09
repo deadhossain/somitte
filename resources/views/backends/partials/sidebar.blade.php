6e8a588632666168d624d86035be69c5d94fbdc5
deadhossain <md.hossain8121@gmail.com>
2021-05-09 08:17:51 +0600

    sidebar user only

--- resources/views/backends/partials/sidebar.blade.php
+++ resources/views/backends/partials/sidebar.blade.php
@@ -1,58 +1,13 @@
 <nav class="pcoded-navbar">
     <div class="pcoded-inner-navbar main-menu">
-
-
-
-
-
-
-
-        <div class="pcoded-navigatio-lavel">Other</div>
+        <div class="pcoded-navigatio-lavel">Setups</div>
         <ul class="pcoded-item pcoded-left-item">
-            <li class="pcoded-hasmenu ">
-                <a href="javascript:void(0)">
-                    <span class="pcoded-micon"><i class="feather icon-list"></i></span>
-                    <span class="pcoded-mtext">Menu Levels</span>
-                </a>
-                <ul class="pcoded-submenu">
-                    <li class="">
-                        <a href="javascript:void(0)">
-                            <span class="pcoded-mtext">Menu Level 2.1</span>
-                        </a>
-                    </li>
-                    <li class="pcoded-hasmenu ">
-                        <a href="javascript:void(0)">
-                            <span class="pcoded-mtext">Menu Level 2.2</span>
-                        </a>
-                        <ul class="pcoded-submenu">
-                            <li class="">
-                                <a href="javascript:void(0)">
-                                    <span class="pcoded-mtext">Menu Level 3.1</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="">
-                        <a href="javascript:void(0)">
-                            <span class="pcoded-mtext">Menu Level 2.3</span>
-                        </a>
-                    </li>
-
-                </ul>
-            </li>
-            <li class="">
-                <a href="javascript:void(0)" class="disabled">
-                    <span class="pcoded-micon"><i class="feather icon-power"></i></span>
-                    <span class="pcoded-mtext">Disabled Menu</span>
-                </a>
-            </li>
             <li class="active">
                 <a href="sample-page.htm">
-                    <span class="pcoded-micon"><i class="feather icon-watch"></i></span>
-                    <span class="pcoded-mtext">Sample Page</span>
+                    <span class="pcoded-micon"><i class="feather icon-users"></i></span>
+                    <span class="pcoded-mtext">Users</span>
                 </a>
             </li>
         </ul>
-
     </div>
 </nav>
