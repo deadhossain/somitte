7ba1870864e2489ff9ca16c3c1f680fbce1d9de2
deadhossain <md.hossain8121@gmail.com>
2021-12-05 19:03:51 +0600

    loan account init

---
+++ app/Models/loan/LoanAccount.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\loan;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LoanAccount extends Model
+{
+    use HasFactory;
+}
